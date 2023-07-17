45bea62f616ec3b20d3169c7afc43510c5480f45
viewsdevelop <viewsdevelop@gmail.com>
2023-07-16 23:22:43 -0400

    Develop main landing page with logo and away message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img id="vendr-logo" src="/vendr-logo.png" alt="Logo" />
+
+        <br />
+        <p className="welcome-text">Coming Soon</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
