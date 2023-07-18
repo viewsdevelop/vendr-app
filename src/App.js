fbde5abe2b849b45b85d176fb8e4e8d92a9e8b06
viewsdevelop <viewsdevelop@gmail.com>
2023-07-17 20:12:32 -0400

    Update image path for main logo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img id="vendr-logo" src="../vendr-logo.png" alt="Logo" />
+        <img id="vendr-logo" src="./vendr-logo.png" alt="Logo" />
 
         <br />
         <p className="welcome-text">Coming Soon</p>
